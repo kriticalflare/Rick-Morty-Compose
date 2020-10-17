f03889fd3c0b366f8804a86ac0dd54e16ebc429c
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-10-17 21:32:28 +0530

    Replace compose-router with compose-navigation

--- app/src/main/java/com/kriticalflare/rickmorty/MainActivity.kt
+++ app/src/main/java/com/kriticalflare/rickmorty/MainActivity.kt
@@ -1,40 +1,47 @@
 package com.kriticalflare.rickmorty
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.setContent
-import com.kriticalflare.rickmorty.characters.CharactersViewModel
-import com.kriticalflare.rickmorty.ui.RickmortyTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
-import com.github.zsoltk.compose.backpress.BackPressHandler
+import androidx.compose.ui.platform.setContent
+import androidx.core.os.bundleOf
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.kriticalflare.rickmorty.characters.CharacterDetailScreen
 import com.kriticalflare.rickmorty.characters.CharacterScreen
-import com.kriticalflare.rickmorty.routing.Router
+import com.kriticalflare.rickmorty.routing.Screen
+import com.kriticalflare.rickmorty.ui.RickmortyTheme
 
 class MainActivity : AppCompatActivity() {
-    private val backPressHandler = BackPressHandler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Providers(
-                AmbientBackPressHandler provides backPressHandler
-            ) {
-                RickmortyTheme {
-                    Router.Content()
+            RickmortyTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.CharacterScreen.title
+                ) {
+                    composable(Screen.CharacterScreen.title) {
+                        CharacterScreen(
+                            Modifier.fillMaxSize(),
+                            onCharSelect = { id ->
+                                navController.navigate(
+                                    Screen.CharacterDetailScreen.title,
+                                    bundleOf("id" to id)
+                                )
+                            })
+                    }
+
+                    composable(Screen.CharacterDetailScreen.title) { navBackStackEntry ->
+                        CharacterDetailScreen(navBackStackEntry.arguments?.getInt("id")!!)
+                    }
                 }
             }
-        }
-    }
-
-    override fun onBackPressed() {
-        if (!backPressHandler.handle()) {
-            super.onBackPressed()
         }
     }
 }
